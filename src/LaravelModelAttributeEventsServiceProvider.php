43d03745dda825fc13ba30b3ecc4c1b7f8b6d8a7
Wali Razzaq <m.wali.razzaq@gmail.com>
2021-11-18 19:02:59 +0500

    configure package

---
+++ src/LaravelModelAttributeEventsServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Walirazzaq\LaravelModelAttributeEvents;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Walirazzaq\LaravelModelAttributeEvents\Commands\LaravelModelAttributeEventsCommand;
+
+class LaravelModelAttributeEventsServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('laravel-model-attribute-events');
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Walirazzaq\LaravelModelAttributeEvents\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+use Walirazzaq\LaravelModelAttributeEvents\LaravelModelAttributeEventsServiceProvider;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Walirazzaq\\LaravelModelAttributeEvents\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaravelModelAttributeEventsServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_laravel-model-attribute-events_table.php.stub';
+        $migration->up();
+        */
+    }
+}
